802add4299c84231c924f9fa0265fdd83ed98563
ibratabian17 <ibratabian@gmail.com>
2023-04-15 19:51:57 +0700

    w

--- jdnd.js
+++ jdnd.js
@@ -1,13 +1,4 @@
-var steam = document.querySelector('.button.steam');
-steam.addEventListener('click', function() {
-  window.location.href = 'https://cdn.bc5f39.workers.dev/download/steam/JD2017.exe';
-});
-var uplay = document.querySelector('.button.uplay');
-uplay.addEventListener('click', function() {
-  window.location.href = 'https://cdn.bc5f39.workers.dev/download/uplay/JD2017.exe';
-});
-
-var customui = document.querySelector('.button.customui');
-customui.addEventListener('click', function() {
-  window.location.href = 'https://cdn.bc5f39.workers.dev/download/customui/bundle';
+var eas = document.querySelector('.easteregg');
+eas.addEventListener('click', function() {
+  window.location.href = 'https://www.youtube.com/watch?v=W9cvXlsfWa8';
 });
